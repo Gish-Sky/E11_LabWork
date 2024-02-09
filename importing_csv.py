2bb3b51290098c4e43e0f0fb085102a091f678d1
Jade Mon <jademon@berkeley.edu>
2024-02-09 14:42:43 -0800

    Update importing_csv.py

--- importing_csv.py
+++ importing_csv.py
@@ -1,3 +1,11 @@
 import csv
+import time
 meta_data = ["Time", "PM2.5", "PM10"] #this is our column values
 file = open("aq_data.csv", "w", newline='')
+writer = csv.writer(file)
+writer.writerow(meta_data)
+while True:
+    now = time.time()
+    data = pm25.read()
+    data_out [now, data["pm 2.5"], data ['pm 10']]
+    writer.writerow(data_out)
