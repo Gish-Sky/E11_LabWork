7e2f4bfd99b58cf38294a12c999a751fd48ea9d5
Jade Mon <jademon@berkeley.edu>
2024-03-22 15:19:34 -0700

    Update Lab_Week_10.py

--- Lab_Week_10.py
+++ Lab_Week_10.py
@@ -1,32 +1,37 @@
 import RPi.GPIO as GPIO
 import time
 import csv
+import sys
 
 counts = 0
+timing = 120
+interval = 10
+start = time.time()
+dif = 0
 
-def my_callback(channel, timing, interval, filename):
-    filename = '{filename}.csv'
-    filename = sys.argv[2]
-    file = open(filename, "w", newline = '')
-    writer = csv.writer(file)
-    writer.writerow(["Timestamp", "Counts"])
+filename = '{filename}.csv'
+filename = sys.argv[2]
+file = open(filename, "w", newline = '')
+writer = csv.writer(file)
+writer.writerow(["Timestamp", "Counts"])
+
+def my_callback(channel):
     print(f"There was a count detected at {time.time()}" )
     global counts
-    while counts<timing:
-        counts = counts + 1
-        timestamp = time.time()
-        print(f"There was a count detected at", timestamp)
-        if counts%interval==0:
-            actual_count = counts/interval
-            writer.writerow([actual_count, timestamp])
+    counts=counts+1
     
 GPIO.setmode(GPIO.BCM)
 GPIO.setup(17, GPIO.IN)
 GPIO.add_event_detect(17, GPIO.FALLING, callback=my_callback)
 
-while True:
+while dif<timing:
     time.sleep(10)  # wait 10 ms to give CPU chance to do other things
     print(f"Number of Counts is {counts}")
+    timestamp = time.time()
+    dif = start-timestamp
+    if dif%interval==0:
+        actual_count = counts//interval
+        writer.writerow([actual_count, timestamp])
 
 
 
